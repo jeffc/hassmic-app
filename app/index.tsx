d5dc8fbae372c92f690a360fa50a5bfbcef18205
Jeff Cooper <jeff@thejeffcooper.com>
2024-08-21 11:48:36 -0400

    got permissions / build stuff worked out.
    `npx expo run:android` to rebuild after native code changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,41 @@
-import { Text, View } from "react-native";
+import {
+  Button,
+  PermissionsAndroid,
+  Text,
+  View,
+} from "react-native";
+
+import { useState } from "react";
+
+import MicStream from "react-native-microphone-stream";
+
 
 export default function Index() {
+  let requestPermission = async () => {
+    let res = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO);
+    console.log(res);
+  };
+
+  const [micOn, setMicOn] = useState(false);
+
+  const listener = MicStream.addListener(data => {});
+  MicStream.init({
+    bufferSize: 4096,
+    sampleRate: 44100,
+    bitsPerChannel: 16,
+    channelsPerFrame: 1,
+  });
+
+  let startStream = async () => {
+    MicStream.start();
+    setMicOn(true);
+  };
+
+  let stopStream = async() => {
+    MicStream.stop();
+    setMicOn(false);
+  };
+
   return (
     <View
       style={{
@@ -9,7 +44,17 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Button 
+          title="Get Permissions"
+          onPress={requestPermission}
+        />
+        <Button 
+          title={ micOn ? "Stop" : "Start" }
+          onPress={() => { micOn ? stopStream() : startStream(); }}
+        />
+      </>
     </View>
   );
 }
